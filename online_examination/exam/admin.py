931786fe39e8d75e72384269ac08f099f30cf6c7
Anoop Antony <anoop@technomicssolutions.com>
2015-02-28 09:11:13 +0530

    changes

--- online_examination/exam/admin.py
+++ online_examination/exam/admin.py
@@ -3,8 +3,6 @@ from exam.models import *
 
 admin.site.register(Subject)
 admin.site.register(Exam)
-admin.site.register(SubjectMark)
-admin.site.register(StudentMark)
 admin.site.register(Question)
 admin.site.register(Choice)
 admin.site.register(StudentAnswer)
